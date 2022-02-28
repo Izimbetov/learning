6cfd80e78d5389d9503e8d35e143ece943a7eb36
Айдос <aydosizimbetov@icloud.com>
2022-02-28 16:08:34 +0500

    Дашол до jonmmmttutuyt

--- js/app.js
+++ js/app.js
@@ -17,3 +17,63 @@ $('.header-slider').slick({
     },
   ]
 });
+
+$('.about-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay:true,
+  arrows: true,
+  speed: 500,
+
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      }
+    },
+  ]
+});
+
+$('.courses-inner-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay:true,
+  arrows: true,
+  speed: 500,
+
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      }
+    },
+  ]
+});
+
+$('.gallery-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  autoplay:true,
+  arrows: true,
+  speed: 500,
+
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+      }
+    },
+  ]
+});
